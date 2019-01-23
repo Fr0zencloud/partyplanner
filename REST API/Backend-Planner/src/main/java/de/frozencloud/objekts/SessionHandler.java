0ff9c82cc04738d9bb1ad9d5f9d0d8638c5f18d8
Niklas Lausch <support@niki2k1.de>
2019-01-23 13:14:21 +0100

    PLANNER-16: SessionHandler

--- REST API/Backend-Planner/src/main/java/de/frozencloud/objekts/SessionHandler.java
+++ REST API/Backend-Planner/src/main/java/de/frozencloud/objekts/SessionHandler.java
@@ -1,8 +1,11 @@
 package de.frozencloud.objekts;
 
+import java.util.List;
+
 public class SessionHandler {
 	private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	private static int sessionIdLength = 25;
+	private List<String> sessionList;
 	
 	//TODO fill the SessionId in Database with given UserId
 	//TODO have a list from all active sessions and check if the newly created is unique

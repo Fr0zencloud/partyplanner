221cc08191a44c2f36d4d5881be78a46908a4577
Niklas Lausch <support@niki2k1.de>
2019-02-19 11:59:43 +0100

    PLANNER-22
    Added Start and End Time to Meeting

--- app/Controllers/Http/MeetingController.js
+++ app/Controllers/Http/MeetingController.js
@@ -9,13 +9,40 @@ class MeetingController {
         const meetings = await Meeting.all()
         let meetingsObj = meetings.toJSON()        
 
-        Logger.info(meetings.start_date)
-        Logger.info(meetingsObj.start_date)
-
+        for(let i in meetingsObj){
+            let start_date = new Date(meetingsObj[i].start_date)
+            let end_date = new Date(meetingsObj[i].end_date)
+            meetingsObj[i].start_date = getDate(start_date)
+            meetingsObj[i].end_date = getDate(end_date)
+            meetingsObj[i].start_time = getTime(start_date)
+            meetingsObj[i].end_time = getTime(end_date)
+        }
 
         return view.render('main', {
             meetings: meetingsObj
         })
+
+        function getDate(date){
+            let day = date.getDay()
+            let month = date.getMonth()
+            let year = date.getFullYear()
+
+            return day + "." + month + "." + year
+        }
+
+        function getTime(date){
+            let hours = date.getHours()
+            let minutes = date.getMinutes()
+
+            if (hours < 10) {
+                hours = "0" + hours;
+            }
+            if (minutes < 10) {
+                minutes = "0" + minutes;
+            }
+
+            return hours + ":" + minutes
+        }
     }
 }
 

9451c55991db9ba87b6063b8b64e044dcf7ca40c
Niklas Lausch <31246997+Niki2k1@users.noreply.github.com>
2019-03-09 19:45:17 +0100

    PLANNER-40: Invitation System
    Added
     * Simplified meeting item
        - Is a simplified version from the Item View
     * InvitationManager Admin Section
        - Added Form for creating Invitations
     * Unread Invitations View
        - Displays the simplified Meeting items to accept/deny
    
    Updated
     * Admin Controller
        - Added InvitationManage Function
        - Changed users from static array to all Users
     * Invitation Controller
        - Added Index Function
        - Added Create Function
        - Added Delete Function
        - Added Close Function
     * Meeting Controller
         - Changed Meeting Query
         - Cleaned up a little bit
         - Changed max participants Logic
         - Added Invitation Count (Sidebar Logic)
         - Added users to invite
         - Added Invitation on Store
     * Style.css
         - Added back class
     * Admin Index View
        - Added List for Links
     * Sidebar View
        - Added Invitation Link
        - Added Invitation Count Badge
     * Item View
        - Changed max participants selector
     * Add View
        - Added Invitation Form Fields
        - Added Invitation Fomr Field JS Logic
     * Detail View
         - Changed Back Link to redirect back (not static)
    * Routes
       - Invitation Routes (Index, Create, Close, Delete)
       - Admin Invitation Manager

--- Webapp/app/Controllers/Http/AdminController.js
+++ Webapp/app/Controllers/Http/AdminController.js
@@ -1,15 +1,35 @@
 'use strict'
 
+const User = use('App/Models/User')
+const Database = use('Database')
+
 class AdminController {
     async index({ view }) {
         return view.render('admin.index')
     }
 
     async usermanage({ view }) {
-        let users = ['Niki2k1', 'Tipet']
+        const users = (await User.all()).toJSON()
 
         return view.render('admin.usermanage', {
             users: users,
+        })
+    }
+
+    async invitationmanage({ view }) {
+        const users = (await User.all()).toJSON()
+        
+        let now = new Date(Date.now())
+
+        const meetings = await Database
+            .select()
+            .from('meetings')
+            .where('start_date', '>', now)
+            .orderBy('start_date', 'asc')
+
+        return view.render('admin.invitationmanage', {
+            users: users,
+            meetings: meetings
         })
     }
 
--- Webapp/app/Controllers/Http/InvitationController.js
+++ Webapp/app/Controllers/Http/InvitationController.js
@@ -1,6 +1,100 @@
 'use strict'
 
+const Meeting = use('App/Models/Meeting')
+const Database = use('Database')
+const Participate = use('App/Models/Participate')
+const Invitation = use('App/Models/Invitation')
+const Logger = use('Logger')
+
 class InvitationController {
+    async index({ view, auth }) {
+       let now = new Date(Date.now())
+       now = now.toISOString().slice(0, 19).replace('T', ' ')
+
+       const meetings = await Database
+        .select('meetings.*', 'invitations.id as invitationId', 'invitations.user_id')
+        .from('meetings')
+        .leftJoin('invitations', 'meetings.id', 'invitations.meeting_id')
+        .where('invitations.user_id', '=', (await auth.getUser()).id)
+
+        meetings.forEach(meeting => {
+            let start_date = new Date(meeting.start_date)
+            let end_date = new Date(meeting.end_date)
+
+            meeting.start_date = getDate(start_date)
+            meeting.end_date = getDate(end_date)
+            meeting.start_time = getTime(start_date)
+            meeting.invitation_id = meeting.invitationId
+        });
+
+        let invitation_count = (await Invitation.query()
+        .where('user_id', '=', (await auth.getUser()).id)
+        .count())[0]['count(*)']
+
+        return view.render('invitations.unread', {
+            meetings:meetings,
+            invitation_count:invitation_count,
+            invitation:{
+                exists: true,
+            }
+        })
+    }
+
+    async close({ params, response, session, auth }) {
+        const invitation = await Invitation.find(params.id)
+        let user_id = (await auth.getUser()).id
+
+        const toParticipate = new Participate()
+        toParticipate.meeting_id = invitation.meeting_id
+        toParticipate.user_id = user_id
+        await toParticipate.save()
+
+        await invitation.delete()
+        
+        session.flash({ notification: 'You are now participating for: ' + (await Meeting.find(invitation.meeting_id)).name })
+
+        return response.redirect('back')
+    }
+
+    async delete({ params, response, session }) {
+        const invitation = await Invitation.find(params.id)
+        await invitation.delete()
+        
+        session.flash({ notification: 'You have denied the invitation!'})
+        return response.redirect('back')
+    }
+
+    async create({ request, response, session }) {
+        const invitation = new Invitation()
+        invitation.user_id = request.input('user_id')
+        invitation.meeting_id = request.input('meeting_id')
+        await invitation.save()
+
+        session.flash({ notification: 'Invitation created' })
+        
+        return response.redirect('back')
+    }
+}
+
+function getDate(date) {
+    let day = date.getDate()
+    let month = date.getMonth() + 1
+    let year = date.getFullYear()
+    return day + "." + month + "." + year
+}
+
+function getTime(date) {
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    return hours + ":" + minutes
 }
 
 module.exports = InvitationController
